f26d81e26747c908207897211aceb6a0d4e49138
unknown <hadycas@gmail.com>
2025-11-12 10:40:29 +0600

    #3.20

--- app/features/products/pages/products-page.tsx
+++ app/features/products/pages/products-page.tsx
@@ -1,28 +1,5 @@
-import type { Router } from "react-router";
-import type { Route } from "./+types/products-page";
+import { redirect } from "react-router";
 
-export function loader({ request, params }: Route.LoaderArgs) {
-  return {};
+export function loader() {
+  return redirect("/products/leaderboards");
 }
-
-export async function action({ request, params }: Route.ActionArgs) {
-  return {};
-}
-
-export const meta: Route.MetaFunction = () => [
-  { title: "Products" },
-  { name: "description", content: "Explore products" },
-];
-
-export default function Component({
-  loaderData,
-  actionData,
-}: Router.ComponentProps<typeof loader, typeof action>) {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-semibold">Products</h1>
-    </div>
-  );
-}
-
-
--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
 import Navigation from "./common/components/navigation";
+import { Settings } from "luxon";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -26,6 +27,9 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  Settings.defaultLocale = "ko";
+  Settings.defaultZone = "Asia/Seoul";
+
   return (
     <html lang="en" className="dark">
       <head>
@@ -35,7 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <main className="px-20">{children}</main>
         <ScrollRestoration />
         <Scripts />
       </body>
